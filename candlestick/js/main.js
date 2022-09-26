3a4eadd20911c334b7892b864465eb4e266525c3
Yu Jia <rockjia1996@gmail.com>
2022-09-26 17:49:42 -0400

    Add a candlechart prototype.

--- candlestick/js/main.js
+++ candlestick/js/main.js
@@ -1,18 +1,20 @@
 const margin = { left: 100, top: 10, right: 10, bottom: 100 }
-const width = 400;
-const height = 300;
+const width = 500;
+const height = 400;
 
 
-const outerCanvas = d3.select("#candle-chart");
-outerCanvas.attr("width", width + margin.left + margin.right)
-outerCanvas.attr("height", height + margin.top + margin.bottom)
-const innerCanvas = outerCanvas.append("g");
+const outerCanvas = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+const innerCanvas = outerCanvas.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .attr("width", width)
+    .attr("height", height);
 
 const xAxisGroup = innerCanvas.append("g");
 const yAxisGroup = innerCanvas.append("g");
 
-const xScale = d3.scaleBand().padding(0.2)
-const yScale = d3.scaleLinear()
+
 
 
 
@@ -20,24 +22,108 @@ d3.csv('data/JNJ.csv').then(rawData => {
     const data = [];
 
     // Format the raw data
-    rawData.forEach(d => {
-        const { Open, High, Low, Close, Volume, Date:date } = d;
+    rawData.slice(1*356).forEach(d => {
+        const { Open, High, Low, Close, Volume, Date: date } = d;
         data.push({
             open: Number(Open),
             high: Number(High),
             low: Number(Low),
             close: Number(Close),
             volume: Number(Volume),
-            date: new Date(date) 
+            date: new Date(date)
         });
     })
     console.log(data)
 
+    const xDate = d3.map(data, d => d.date)
+    const yOpen = d3.map(data, d => d.open)
+    const yClose = d3.map(data, d => d.close)
+    const yHigh = d3.map(data, d => d.high)
+    const yLow = d3.map(data, d => d.low)
+    const I = d3.range(xDate.length)
+
+    let xDomain = undefined;
+    let xRange = [0, width];
+    let yDomain = undefined;
+    let yRange = [height, 0];
+    let xTicks = undefined;
+
+    const weeks =
+        (start, stop, stride) =>
+            d3.utcMonday.every(stride).range(start, +stop + 1);
+    const weekdays =
+        (start, stop) =>
+            d3.utcDays(start, +stop + 1)
+                .filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6);
+
+    if (xDomain === undefined)
+        xDomain = weekdays(d3.min(xDate), d3.max(xDate));
+    if (yDomain === undefined)
+        //yDomain = [d3.min(yLow), d3.max(yHigh)];
+        yDomain = [0, d3.max(yHigh)];
+    if (xTicks === undefined)
+        xTicks = weeks(d3.min(xDomain), d3.max(xDomain), 2);
+
+    const xScale = d3.scaleBand().domain(xDomain).range(xRange).padding(0.2).paddingOuter(0.2);
+    const yScale = d3.scaleLinear().domain(yDomain).range(yRange);
+
+    const xAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.utcFormat("%b %-d"))
+        .tickValues([
+            xTicks[0],
+            xTicks[Math.ceil(xTicks.length * 0.25)],
+            xTicks[Math.ceil(xTicks.length * 0.5)],
+            xTicks[Math.ceil(xTicks.length * 0.75)],
+            xTicks[xTicks.length - 1],
+        ]);
+
+    const yAxis = d3.axisLeft(yScale).ticks(height / 40, "~f")
 
 
+    xAxisGroup
+        .attr("transform", `translate(0, ${height + 20})`)
+        .call(xAxis)
+        .call(g => g.select(".domain").remove())
+
+    yAxisGroup
+        .attr("transform", `translate(-20, 0)`)
+        .call(yAxis)
+        .call(g => g.select(".domain").remove())
+
+    const g = innerCanvas.append("g")
+        .attr("stroke", "currentColor")
+        .attr("stroke-linecap", "round")
+        .selectAll("g")
+        .data(I)
+        .join("g")
+        .attr("transform", i => `translate(${xScale(xDate[i])},0)`);
+
+    g.append("line")
+        .attr("y1", i => {
+            //console.log(yScale(yLow[i]))
+            return yScale(yLow[i])
+        })
+
+        .attr("y2", i => {
+            //console.log(yScale(yHigh[i]))
+            return yScale(yHigh[i])
+        });
+
+    colors = ["#4daf4a", "#999999", "#e41a1c"]
+    g.append("line")
+        .attr("y1", i => {
+            console.log(yScale(yOpen[i]))
+            console.log(yOpen[i])
+            console.log(yScale(0))
+            console.log(yScale.domain())
+
+            return yScale(yOpen[i])
+        })
+        .attr("y2", i => yScale(yClose[i]))
+        .attr("stroke-width", xScale.bandwidth())
+        .attr("stroke", i => colors[1 + Math.sign(yLow[i] - yClose[i])]);
 
 
 })
-
 
 
