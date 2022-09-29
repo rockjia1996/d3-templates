5c2e4a222726bdbc0de5664f0d484780380214b3
Yu Jia <rockjia1996@gmail.com>
2022-09-29 16:47:22 -0400

    Make the chart interactive when user by allowing user to select different segment of data to display.

--- candlestick/js/CandleStick.js
+++ candlestick/js/CandleStick.js
@@ -11,7 +11,7 @@ class CandleStick {
         this.yScale = d3.scaleLinear();
 
         this.data = this.dataCleaning(data);
-        this.presentingData = this.data;
+        this.presentingData = this.data.slice(-180);
         this.colors = ["#4daf4a", "#999999", "#e41a1c"];
 
         this.outerCanvas = d3.select(chartArea).append("svg")
@@ -52,29 +52,14 @@ class CandleStick {
         this.yAxisGroup
             .attr("transform",
                 `translate(${this.width + this.marginRight / 2}, 0)`)
+
         this.render()
 
     }
 
     // Doing data join, enter, update, exit
     render() {
-
-        /*
-        this.selectTimePeriod(
-            this.presentingData,
-            d3.min(this.presentingData, d =>d.date),
-            d3.max(this.presentingData, d =>d.date),
-            this.presentingData.length <= 180
-            ? 1 
-            : Math.ceil(this.presentingData.length / 180)
-        );
-        
-        */
-        // Configure x and y domain
-
-
         const xDomain = this.presentingData.map(d => d.date);
-        console.log(this.presentingData)
         const yDomain = [
             this.findMin(this.presentingData),
             this.findMax(this.presentingData)
@@ -97,12 +82,6 @@ class CandleStick {
             .call(g => g.select(".domain").remove())
 
 
-        console.log(this.yScale.ticks())
-
-
-
-
-
         /* Candle sticks: Data join, enter, update, exit */
         const candles = this.chart.selectAll("g")
             .data(this.presentingData, d => d.date)
@@ -110,9 +89,11 @@ class CandleStick {
 
         candles.exit().remove();
 
+        // Enter
         const newCandles = candles.enter()
             .append("g")
-            .attr("transform", d => `translate(${this.xScale(d.date) + this.xScale.bandwidth() / 2}, 0)`)
+            .attr("transform", d =>
+                `translate(${this.xScale(d.date) + this.xScale.bandwidth() / 2}, 0)`)
             .on("mouseover", (event, data) => {
                 event.target.setAttribute("fill-opacity", "0.6")
                 event.target.setAttribute("fill", "grey")
@@ -122,7 +103,8 @@ class CandleStick {
                 event.target.setAttribute("fill", "transparent")
             })
 
-        newCandles.append("title")
+        newCandles
+            .append("title")
             .text(d => {
                 let tooltip =
                     `Open ----- ${d.open}\n` +
@@ -134,9 +116,9 @@ class CandleStick {
                 return tooltip;
             })
 
-        // Need to special case like BRK.A
         newCandles
             .append("line")
+            .attr("class", "high-low")
             .attr("y1", d => {
                 if (d.low === d.high)
                     return this.yScale(d.low) - 1.5;
@@ -149,8 +131,10 @@ class CandleStick {
                 else
                     return this.yScale(d.high)
             })
+
         newCandles
             .append("line")
+            .attr("class", "open-close")
             .attr("y1", d => this.yScale(d.open))
             .attr("y2", d => this.yScale(d.close))
             .attr("stroke-width", this.xScale.bandwidth())
@@ -164,6 +148,34 @@ class CandleStick {
             .attr("stroke", "none")
             .attr("fill", "transparent")
 
+        // Update
+        candles
+            .attr("transform", d =>
+                `translate(${this.xScale(d.date)+this.xScale.bandwidth()/2}, 0)`)
+
+        candles.selectAll(".high-low")
+            .attr("y1", d => {
+                if (d.low === d.high) return this.yScale(d.low) - 1.5;
+                else return this.yScale(d.low)
+            })
+            .attr("y2", d => {
+                if (d.low === d.high) return this.yScale(d.high) + 1.5
+                else return this.yScale(d.high)
+            })
+
+        candles
+            .selectAll(".open-close")
+            .attr("y1", d => this.yScale(d.open))
+            .attr("y2", d => this.yScale(d.close))
+            .attr("stroke-width", this.xScale.bandwidth())
+            .attr("stroke", d => this.colors[1 + Math.sign(d.open - d.close)])
+
+        candles.selectAll("rect")
+            .attr("width", this.xScale.bandwidth())
+            .attr("height", this.height)
+            .attr("transform", `translate(${-this.xScale.bandwidth() / 2}, 0)`)
+            .attr("stroke", "none")
+            .attr("fill", "transparent")
 
 
         /* Y axis gridline: Data join, enter, update, exit */
@@ -280,14 +292,64 @@ class CandleStick {
         return [first, q1, median, q3, last]
     }
 
+    queryTimePeriod(period, weekends = false) {
+
+        const findStartDate = (period) => {
+            const bisect = d3.bisector(d => d.date);
+            const latest = this.data.slice(-1)[0].date;
+            const month = latest.getMonth();
+            const year = latest.getFullYear();
+            const day = latest.getDay();
+            let startDate = undefined;
+
+            if (period === "1d") return this.data.at(-2).date;
+            if (period === "5d") return this.data.at(-5).date;
+            if (period === "1m") return this.data.at(-31).date;
+
+            if (period === "6m") {
+                startDate = (month - 6) >= 0
+                    ? new Date(`${year}-${month - 6}-${day}`)
+                    : new Date(`${year}-${(month + 6)}-${day}`)
+            }
+
+            if (period === "ytd") {
+                startDate = new Date(`${year}-01-01`);
+            }
+
+            if (period === "1y") {
+                startDate = new Date(`${year - 1}-${month}-${day}`);
+            }
+
+            if (period === "5y") {
+                startDate = new Date(`${year - 5}-${month}-${day}`);
+            }
+
+            if (period === "max") {
+                return this.data.at(0).date;
+            }
+
+            const index = bisect.left(this.data, startDate);
+            return this.data[index].date;
+
+        }
+
+        this.presentingData = this.selectTimePeriod(
+            this.data,
+            findStartDate(period),
+            this.data.slice(-1)[0].date);
+        this.render();
+    }
+
+
 
     selectTimePeriod(data, start, end, weekends = false) {
+        // Find the closest dates to the specificed dates
+
         const startIndex = data.findIndex(
             d => d.date.getTime() === start.getTime());
         const endIndex = data.findIndex(
             d => d.date.getTime() === end.getTime());
 
-        console.log(startIndex)
         if (weekends)
             return data.slice(startIndex, endIndex + 1);
         else
@@ -297,11 +359,11 @@ class CandleStick {
 
 
 
-    dailySnapshot(start, end, stride = 1) {
+    days(start, end, stride = 1) {
         return d3.utcDays(start, +end + 1, stride);
     }
 
-    weekdaysSnapshot(start, end, stride = 1) {
+    weekdays(start, end, stride = 1) {
         return d3.utcDays(start, +end + 1, stride)
             .filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6);
     }
@@ -326,7 +388,7 @@ d3.csv('data/AAPL.csv').then(rawData => {
     const data = [];
 
     // Format the raw data
-    rawData.slice(-365).forEach(d => {
+    rawData.forEach(d => {
         const { Open, High, Low, Close, Volume, Date: date } = d;
         data.push({
             open: Number(Open),
@@ -348,6 +410,20 @@ d3.csv('data/AAPL.csv').then(rawData => {
     }
 
     const chart = new CandleStick("#chart", dims, {}, {}, data);
+    //chart.queryTimePeriod("6m")
+    //chart.queryTimePeriod("1m")
+
+
+
+    const btns = document.querySelector('#btns').children
+    btns[0].onclick = () => chart.queryTimePeriod("1d")
+    btns[1].onclick = () => chart.queryTimePeriod("5d")
+    btns[2].onclick = () => chart.queryTimePeriod("1m")
+    btns[3].onclick = () => chart.queryTimePeriod("6m")
+    btns[4].onclick = () => chart.queryTimePeriod("ytd")
+    btns[5].onclick = () => chart.queryTimePeriod("1y")
+    btns[6].onclick = () => chart.queryTimePeriod("5y")
+    btns[7].onclick = () => chart.queryTimePeriod("max")
 
 })
 
