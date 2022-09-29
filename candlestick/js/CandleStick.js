1b77f087b3e4e600a9dfffb1acdcc5b10e4b91c1
Yu Jia <rockjia1996@gmail.com>
2022-09-29 13:17:12 -0400

    Add gridline.

--- candlestick/js/CandleStick.js
+++ candlestick/js/CandleStick.js
@@ -24,7 +24,7 @@ class CandleStick {
                     this.width + this.marginRight + this.marginRight,
                     this.height + this.marginTop + this.marginBottom
                 ])
-            .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+            .attr("style", `max-width: 100%; height: auto; height: intrinsic;background-color:#f4f4f4;`)
 
 
         this.innerCanvas = this.outerCanvas.append("g")
@@ -39,39 +39,71 @@ class CandleStick {
         this.xAxisGroup = this.innerCanvas.append("g");
         this.yAxisGroup = this.innerCanvas.append("g");
 
-        const xDomain = this.weekdaysSnapshot(
-            d3.min(this.data, d => d.date),
-            d3.max(this.data, d => d.date)
-        );
+        this.yGridLineGroup = this.innerCanvas.append("g");
 
         const xRange = [0, this.width];
-        const yDomain = [this.findMin(this.data), this.findMax(this.data)];
         const yRange = [this.height, 0];
-        this.xScale.domain(xDomain).range(xRange);
-        this.yScale.domain(yDomain).range(yRange);
-
-        this.xAxisCall = d3.axisBottom(this.xScale)
-            .tickFormat(d3.utcFormat("%b %-d"))
-            .tickValues(this.xTicksValues(xDomain, 10));
-        this.yAxisCall = d3.axisLeft(this.yScale);
-
+        this.xScale.range(xRange);
+        this.yScale.range(yRange);
 
         this.xAxisGroup
-            .attr("transform", `translate(0, ${this.height})`)
-            .call(this.xAxisCall)
-            .call(g => g.select(".domain").remove())
-
+            .attr("transform",
+                `translate(0, ${this.height + this.marginBottom / 4})`)
         this.yAxisGroup
-            .call(this.yAxisCall)
-            .call(g => g.select(".domain").remove())
-
-
+            .attr("transform",
+                `translate(${this.width + this.marginRight / 2}, 0)`)
         this.render()
 
     }
 
     // Doing data join, enter, update, exit
     render() {
+
+        /*
+        this.selectTimePeriod(
+            this.presentingData,
+            d3.min(this.presentingData, d =>d.date),
+            d3.max(this.presentingData, d =>d.date),
+            this.presentingData.length <= 180
+            ? 1 
+            : Math.ceil(this.presentingData.length / 180)
+        );
+        
+        */
+        // Configure x and y domain
+
+
+        const xDomain = this.presentingData.map(d => d.date);
+        console.log(this.presentingData)
+        const yDomain = [
+            this.findMin(this.presentingData),
+            this.findMax(this.presentingData)
+        ];
+        this.xScale.domain(xDomain);
+        this.yScale.domain(yDomain);
+
+        // Configure x and y axis
+        const xAxisCall = d3.axisBottom(this.xScale)
+            .tickFormat(d3.utcFormat("%b %-d"))
+            .tickValues(this.xTicksValues(xDomain, 10));
+        const yAxisCall = d3.axisRight(this.yScale);
+
+        this.xAxisGroup
+            .call(xAxisCall)
+            .call(g => g.select(".domain").remove())
+
+        this.yAxisGroup
+            .call(yAxisCall)
+            .call(g => g.select(".domain").remove())
+
+
+        console.log(this.yScale.ticks())
+
+
+
+
+
+        /* Candle sticks: Data join, enter, update, exit */
         const candles = this.chart.selectAll("g")
             .data(this.presentingData, d => d.date)
         //transform to the correct x positions based on the x scale
@@ -132,6 +164,40 @@ class CandleStick {
             .attr("stroke", "none")
             .attr("fill", "transparent")
 
+
+
+        /* Y axis gridline: Data join, enter, update, exit */
+        const yGridLine = this.yGridLineGroup.selectAll("line")
+            .data(this.yScale.ticks())
+
+        yGridLine.exit().remove()
+
+        yGridLine.enter().append("line")
+            .transition()
+            .duration(50)
+            .attr("x1", 0)
+            .attr("y1", d => this.yScale(d))
+            .attr("x2", this.width)
+            .attr("y2", d => this.yScale(d))
+            .attr("fill", "none")
+            .attr("shape-rendering", "crispEdges")
+            .attr("stroke", "#b6b6b6")
+            .attr("stroke-width", "1px")
+            .attr("stroke-dasharray", 2)
+
+        yGridLine
+            .transition()
+            .duration(50)
+            .attr("x1", 0)
+            .attr("y1", d => this.yScale(d))
+            .attr("x2", this.width)
+            .attr("y2", d => this.yScale(d))
+            .attr("fill", "none")
+            .attr("shape-rendering", "crispEdges")
+            .attr("stroke", "#b6b6b6")
+            .attr("stroke-width", "1px")
+            .attr("stroke-dasharray", 2)
+
     }
 
     dataCleaning(data) {
@@ -162,6 +228,25 @@ class CandleStick {
             )
         })
         return cleaned;
+    }
+
+    dataFormatChecker() {
+        // To Do: check if data is sorted, contains open, high, low, close,
+        // volume, date
+    }
+
+    dataSnapShot(data, stride = 1) {
+        let target = 0;
+        const snapshots = []
+
+        data.forEach((item, index) => {
+            if (index === target) {
+                snapshots.push(item);
+                target += stride;
+            }
+        })
+
+        return snapshots;
     }
 
     findMax(data) {
@@ -196,11 +281,24 @@ class CandleStick {
     }
 
 
+    selectTimePeriod(data, start, end, weekends = false) {
+        const startIndex = data.findIndex(
+            d => d.date.getTime() === start.getTime());
+        const endIndex = data.findIndex(
+            d => d.date.getTime() === end.getTime());
+
+        console.log(startIndex)
+        if (weekends)
+            return data.slice(startIndex, endIndex + 1);
+        else
+            return data.slice(startIndex, endIndex + 1).filter(
+                d => d.date.getUTCDay() !== 0 && d.date.getUTCDay() !== 6);
+    }
 
 
 
     dailySnapshot(start, end, stride = 1) {
-        return d3.utcDays(start, +end+1, stride);
+        return d3.utcDays(start, +end + 1, stride);
     }
 
     weekdaysSnapshot(start, end, stride = 1) {
@@ -227,9 +325,6 @@ class CandleStick {
 d3.csv('data/AAPL.csv').then(rawData => {
     const data = [];
 
-
-
-
     // Format the raw data
     rawData.slice(-365).forEach(d => {
         const { Open, High, Low, Close, Volume, Date: date } = d;
@@ -243,35 +338,7 @@ d3.csv('data/AAPL.csv').then(rawData => {
         });
     })
 
-
-
-    const selectTimePeriod = (data, start, end, weekends=false) => {
-        const startIndex = data.findIndex(
-            d => d.date.getTime() === start.getTime());
-        const endIndex = data.findIndex(
-            d => d.date.getTime() === end.getTime());
-        
-        console.log(startIndex)
-        if (weekends)
-            return data.slice(startIndex, endIndex+1);
-        else
-            return data.slice(startIndex, endIndex+1).filter(
-                d => d.date.getUTCDay() !== 0 && d.date.getUTCDay() !== 6);
-    }
-
-
-
-    const result = selectTimePeriod(
-        data, 
-        new Date("2022-03-29"), 
-        new Date("2022-09-28")
-    )
-
-    console.log(result)
-
-
-
-    const dimConfigs = {
+    const dims = {
         width: 1200,
         height: 300,
         marginLeft: 100,
@@ -280,14 +347,7 @@ d3.csv('data/AAPL.csv').then(rawData => {
         marginBottom: 100
     }
 
-    const chart = new CandleStick(
-        "#chart",
-        dimConfigs,
-        {},
-        {},
-        result
-    )
-
+    const chart = new CandleStick("#chart", dims, {}, {}, data);
 
 })
 
